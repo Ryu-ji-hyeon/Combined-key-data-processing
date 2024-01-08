83d0e25c3611a43a18a22e866f9907d6e5f6de01
류지현 <dudvndwlgus@naver.com>
2024-01-08 14:22:04 +0900

    수치형 데이터 추출 및 csv 파일 생성

--- numeric_analysis.py
+++ numeric_analysis.py
@@ -1,3 +1,4 @@
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import min, lit, count
 from pyspark.sql import functions as F
 from pyspark.sql.functions import *
@@ -10,7 +11,7 @@ def numeric_data(df, numeric_columns):
 
     for col_name in numeric_columns:
         # 정수 타입의 컬럼에 대해서는 Double 형식으로 변환
-        numeric_summary = numeric_summary.withColumn(col_name, numeric_summary[col_name].cast('float'))
+        numeric_summary = numeric_summary.withColumn(col_name, numeric_summary[col_name].cast('double'))
 
         min_value = numeric_summary.select(min(col_name)).collect()[0][0]
         quantiles1 = numeric_summary.approxQuantile(col_name, [0.25], 0.01)
@@ -20,6 +21,14 @@ def numeric_data(df, numeric_columns):
 
         # 수정: 각 값을 한 번에 추가
         quantiles.append([col_name, min_value, quantiles1, quantiles2, quantiles3, quantiles4])
-        print(quantiles(col_name))
 
+    # 판다스 데이터프레임으로 변환
+    columns = ['col_name', 'min_value', 'quantiles1', 'quantiles2', 'quantiles3', 'quantiles4']
+    pd_df = pd.DataFrame(quantiles, columns=columns)
+
+    # CSV 파일로 저장
+    output_path = "수치형 데이터.csv"
+    pd_df.to_csv(output_path, index=False, encoding='euc-kr') 
+
+    # csv파일 만드는데까지 1시간
     return quantiles
