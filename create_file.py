5d0958096725bafce3bc313b16e0530d0f80ce28
류지현 <dudvndwlgus@naver.com>
2024-01-10 14:18:44 +0900

    5천만건 데이터 생성 , 행 5천만 열 20

--- create_file.py
+++ create_file.py
@@ -2,15 +2,16 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, LongType, DoubleType
 from pyspark.sql import Row
 import numpy as np
+import random
 from pyspark.sql.functions import monotonically_increasing_id
-from pyspark.serializers import ArrowSerializer, PickleSerializer
+
 
 # SparkSession 생성
 spark = SparkSession.builder.appName("GenerateData").getOrCreate()
 
-# 데이터 프레임을 생성할 갯수 (행의 수는 5천만으로, 열의 수는 10으로 줄임)
+# 데이터 프레임을 생성할 갯수 
 num_rows = 50000000
-num_columns = 10  # 열의 수를 10개로 변경
+num_columns = 20  # 열의 수를 20개로 변경
 
 # 데이터 스키마 정의
 schema = StructType([
@@ -18,14 +19,23 @@ schema = StructType([
     StructField("seq_id", LongType(), nullable=False),
 ])
 
-# 열의 수를 10개로 줄임
+# 열의 수를 20개
 for i in range(1, num_columns + 1):
     schema.add(StructField(f'col{i}', DoubleType(), nullable=True))
 
-# id는 랜덤으로 겹치지 않는 수로, seq_id는 1부터 연속적인 수로 생성
+# 초기 데이터
 sample_data = {
     'id': list(range(1, num_rows + 1)),
     'seq_id': list(range(1, num_rows + 1)),
+}
+
+# id를 랜덤으로 섞기
+random_ids = random.sample(sample_data['id'], len(sample_data['id']))
+
+# 결과 데이터
+result_data = {
+    'id': random_ids,
+    'seq_id': sample_data['seq_id'],
 }
 
 # col1~col10은 rand 함수를 사용하여 생성
@@ -34,11 +44,6 @@ for i in range(1, num_columns + 1):
 
 # RDD를 사용하여 데이터프레임 생성
 rdd = spark.sparkContext.parallelize([Row(**sample_data) for _ in range(1)], numSlices=4)
-
-# 직렬화 방법을 Arrow로 변경
-spark.sparkContext._conf.set("spark.serializer", ArrowSerializer().getName())
-spark.sparkContext._conf.set("spark.executor.pyspark.serializer", ArrowSerializer().getName())
-spark.sparkContext._conf.set("spark.driver.pyspark.serializer", ArrowSerializer().getName())
 
 # 예전 코드와 같이 monotonically_increasing_id를 사용하여 id 컬럼 추가
 df = spark.createDataFrame(rdd, schema=schema)
