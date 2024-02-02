fed82ca375ff682f601b63be79311f93e14748a5
류지현 <dudvndwlgus@naver.com>
2024-02-02 17:48:50 +0900

    DataFrame Check

--- 10억건 JOIN/new_10억건 JOIN/function.py
+++ 10억건 JOIN/new_10억건 JOIN/function.py
@@ -3,11 +3,12 @@ from pyspark.sql.functions import *
 
 
 def rdd_iterate(rdd, chunk_size=1000000):
+
     print("성공3")
     indexed_rows = rdd.zipWithIndex().cache()
     print("성공4")
     
-    # 수정: countApprox 메서드의 반환값을 그대로 사용
+    # countApprox 메서드의 반환값을 그대로 사용
     count = indexed_rows.countApprox(1000)
     
     print("성공5")
@@ -21,17 +22,20 @@ def rdd_iterate(rdd, chunk_size=1000000):
         print("Grabbing new chunk: start = {}, end = {}".format(start, end))
         chunk = indexed_rows.filter(lambda r: r[1] >= start and r[1] < end).collect()
         
-        # 수정: DataFrame이 아닌 경우에 대한 체크 추가
+        # DataFrame이 아닌 경우에 대한 체크 추가
         for row in chunk:
             if isinstance(row[0], DataFrame):
                 yield row[0]
             else:
-                print("Not a DataFrame: {}".format(row[0]))
+                # Row 객체를 Spark DataFrame으로 변환
+                spark = SparkSession.builder.getOrCreate()
+                df = spark.createDataFrame([row[0]])
+                yield df
         
         start = end
         end = start + chunk_size
 
-# 수정: rdd_iterate의 반환값을 명시적으로 지정
+# rdd_iterate의 반환값을 명시적으로 지정
 def rdd(rdd):
     return rdd_iterate(rdd)
 
