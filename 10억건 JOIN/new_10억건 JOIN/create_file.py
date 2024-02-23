fbe76a2c861411151af250e7562dfc3a9485c5ca
류지현 <dudvndwlgus@naver.com>
2024-02-23 15:38:16 +0900

    Update drop column

--- 10억건 JOIN/new_10억건 JOIN/create_file.py
+++ 10억건 JOIN/new_10억건 JOIN/create_file.py
@@ -1,31 +1,42 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 import time
-from aaaa import rdd
-spark = SparkSession.builder.appName('example').config("spark.local.dir", "/home/spark-temp").getOrCreate()
+from function import rdd
+spark = SparkSession.builder \
+    .appName('example') \
+    .config("spark.local.dir", "/home/spark-temp") \
+    .config("spark.executor.memory", "8g") \
+    .config("spark.sql.shuffle.partitions", "10000") \
+    .getOrCreate()
 spark.stop()
-spark = SparkSession.builder.appName('example').config("spark.local.dir", "/home/spark-temp").getOrCreate()
+spark = SparkSession.builder \
+    .appName('example') \
+    .config("spark.local.dir", "/home/spark-temp") \
+    .config("spark.executor.memory", "8g") \
+    .config("spark.sql.shuffle.partitions", "10000") \
+    .getOrCreate()
 spark.conf.set("spark.sql.shuffle.partitions", "10000")
 
 if __name__ == "__main__":
     start = time.time()
 
     # 데이터 프레임 생성
-    one = spark.read.csv("/home/data/8억건.csv", header=True, inferSchema=True, encoding='utf-8')
-    one = one.withColumn("seq_id", monotonically_increasing_id())
+    one1 = spark.read.csv("/home/data/8억건.csv", header=True, inferSchema=True, encoding='utf-8')
+    one = one1.selectExpr('key_1210 as key_1210_one','sex_ctgo_cd as sex_ctgo_cd_one', 'cstmr_hom_scls_code as cstmr_hom_scls_code_one',
+                      'job_cd as job_cd_one', 'indv_incm_amt as indv_incm_amt_one', 'seg_34 as seg_34_one',
+                      'sale_date as sale_date_one', 'auth_time as auth_time_one', 'mer_tpbuz_no as mer_tpbuz_no_one',
+                      'mer_rgn_scls_code as mer_rgn_scls_code_one', 'sale_amt as sale_amt_one', 
+                      'column_6 as column_6_one', 'column_8 as column_8_one', 'column_9 as column_9_one',
+                      'column_10 as column_10_one').withColumn("seq_id", monotonically_increasing_id())
     
-    two = spark.read.csv("/home/data/8억건.csv", header=True, inferSchema=True, encoding='utf-8')
-    two = two.withColumn("id", (rand() * 800000000).cast("long"))
-    two = two.withColumnRenamed("key_1210", "key_1210_two").withColumnRenamed("age_val", "age_val_two").withColumnRenamed("sex_ctgo_cd", "sex_ctgo_cd_two").withColumnRenamed("cstmr_hom_scls_code", "cstmr_hom_scls_code_two")\
-             .withColumnRenamed("job_cd", "job_cd_two").withColumnRenamed("indv_incm_amt", "indv_incm_amt_two").withColumnRenamed("seg_34", "seg_34_two").withColumnRenamed("sale_date", "sale_date_two") \
-             .withColumnRenamed("auth_time", "auth_time_two").withColumnRenamed("mer_tpbuz_no", "mer_tpbuz_no_two").withColumnRenamed("mer_rgn_scls_code", "mer_rgn_scls_code_two").withColumnRenamed("sale_amt", "sale_amt_two") \
-             .withColumnRenamed("column_6", "column_6_two").withColumnRenamed("column_8", "column_8_two").withColumnRenamed("column_9", "column_9_two").withColumnRenamed("column_10", "column_10_two") \
-   #  a1_2000 = one.union(two)
-   #  a1_2000 = a1_2000.withColumn("seq_id", monotonically_increasing_id())
+    two1 = spark.read.csv("/home/data/8억건.csv", header=True, inferSchema=True, encoding='utf-8')
+    two = two1.selectExpr('key_1210 as key_1210_two','sex_ctgo_cd as sex_ctgo_cd_two', 'cstmr_hom_scls_code as cstmr_hom_scls_code_two',
+                      'job_cd as job_cd_two', 'indv_incm_amt as indv_incm_amt_two', 'seg_34 as seg_34_two',
+                      'sale_date as sale_date_two', 'auth_time as auth_time_two', 'mer_tpbuz_no as mer_tpbuz_no_two',
+                      'mer_rgn_scls_code as mer_rgn_scls_code_two', 'sale_amt as sale_amt_two', 
+                      'column_6 as column_6_two', 'column_8 as column_8_two', 'column_9 as column_9_two',
+                      'column_10 as column_10_two').withColumn("id", (rand() * 800000000).cast("long"))
 
-   #  aaaaa1_2000 = one.union(two)
-   #  aaaaa1_2000 = aaaaa1_2000.withColumn("id", (rand() * 800000000).cast("long"))
-    
     
     # Create aliases for the DataFrames
     a1_2000_alias = one.alias("a1")
@@ -53,8 +64,8 @@ if __name__ == "__main__":
 
     if result_rdd is not None:
        combined_df = rdd(result_rdd)
+
     
-    combined_df.coalesce(1).write.option("header", "true").csv("/home/data/enenen.csv")
     
     end = time.time()
     print("파일 생성 성공,",f"경과 시간: {int(end - start)} 초")
